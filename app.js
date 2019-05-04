23ab69f9bbcf9a5aad0c33f12c10e8c7be698846
surfwalker <alexanderkreid@gmail.com>
2019-05-03 18:51:15 -0700

    Problem 4 solution code added.

--- app.js
+++ app.js
@@ -80,12 +80,21 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-	
+	var outputSum = sumArr[0];
+	if(sumArr.length > 1) {
+		for(var i = 1; i < sumArr.length; i++) {
+			outputSum = sum(outputSum, sumArr[i])[0];
+		}
+	} else {
+		return "There are not enough items in this array.";
+	}
+	var messageSum = testArray + ' was passed in as an array of numbers, and ' + outputSum + ' is their sum.';
+	return [outputSum, messageSum];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 

24e94ba10bed987351cfc6ba092f01a2147b11b1
surfwalker <alexanderkreid@gmail.com>
2019-05-03 19:03:03 -0700

    Problem6 solution code added

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 	var outputMultiplyAny = dynamicArray[0];
-	if(dynamicArray.length > ) {
+	if(dynamicArray.length > 1) {
 		for(var i = 1; i < dynamicArray.length; i++) {
 			outputMultiplyAny = multiply(outputMultiplyAny, dynamicArray[i])[0];
 		}
